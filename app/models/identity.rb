4f3993716b0d9afabcf6e6e49c04c2adb4815387
Andrew Callahan <andrew.callahan@gmail.com>
2013-04-12 23:42:49 -0400

    add associations to models

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -1,3 +1,5 @@
 class Identity < ActiveRecord::Base
   attr_accessible :asecret, :atoken, :oauth_expires_at, :uid, :user_id
+
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
   attr_accessible :location, :name, :uid
+
+  has_one :identity
+  belongs_to :location
+
+  has_many :friendships
+  has_many :friends, :through => :friendships, :class_name => "User", :foreign_kay => :friend_id
 end
