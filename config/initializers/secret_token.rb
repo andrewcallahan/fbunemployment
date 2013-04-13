6b816e107d9eb94773d6b1cad59b5a3f44c178b8
Andrew Callahan <andrew.callahan@gmail.com>
2013-04-12 23:11:20 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fbunemployment::Application.config.secret_token = 'd54130be190aeae5592664a17cabf9f22b7ed48dc1773a5bc89fa07eb37d7eeff8143cf87ef5f45484e835b110b7f61f60949f4d9c61e4cc9fdb3b149181468f'
